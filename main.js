9dddbd236e8b1af3b6068f32661fe63e51dbf225
Wesley <130110338+wesley1291@users.noreply.github.com>
2024-06-14 08:56:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const caixaPrincipal =document.querySelector('caixa-principal');
+const caixaPerguntas =document.querySelector('caixa-perguntas');
+const caixaAlternativa =document.querySelector('caixa-alternativas');
+const caixaResultado =document.querySelector('caixa-resultado');
+const textoResultado =document.querySelector('texto-resultado');
+
+const perguntas = [
+{
+    enunciado: "assim que saiu da escola voce depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele tambem gera imagems e audios iper-realistas qual o primeiro pensamento?",
+    alternativas: ["isso e assustador","isso e maravilhoso!"]
+},  
+{
+    enunciado: "Insira o enunciado da Pergunta 2",
+    alternativas: ["Alternativa 1","Alternativa 2"]
+}  
+]
